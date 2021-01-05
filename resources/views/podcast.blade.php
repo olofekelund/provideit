dfab110080775939d30f20c13e47ed917477f0cf
Olof Ekelund <olof@kaupa.se>
2021-01-05 11:10:12 +0100

    Add react ui to composer

---
+++ resources/views/podcast.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Podcast finder</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+</head>
+
+<body class="antialiased">
+    <div
+        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+
+    </div>
+    <div class="content">
+        <div id="app"></div>
+    </div>
+</body>
+<script type="text/javascript" src="js/app.js"></script>
+
+</html>
